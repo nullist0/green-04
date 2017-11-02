b69e7c5b9e384cdd447dfb30b8a829ddbf4a91a8
Lee-Null <null.nullist@gmail.com>
2017-11-02 22:59:47 +0900

    modify CardActivity

--- CardNewsApp/app/src/main/java/com/example/ksh/cardnewsapp/CardActivity.java
+++ CardNewsApp/app/src/main/java/com/example/ksh/cardnewsapp/CardActivity.java
@@ -2,16 +2,15 @@ package com.example.ksh.cardnewsapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.ksh.cardnewsapp.adapter.CardPagerAdapter;
 import com.example.ksh.cardnewsapp.data.Card;
@@ -34,10 +33,17 @@ public class CardActivity extends Activity implements View.OnClickListener{
 
     private CardPagerAdapter cpa_main;
     private UltraViewPager uvp_main;
-    private PagerListener pl_main;
 
     private Button bt_image, bt_text, bt_temp;
-    private View view_text, view_temp;
+    private LinearLayout ll_text, ll_temp;
+
+    //ll_text
+    private EditText et_text, et_title;
+    private Button bt_confirm_text;
+
+    //ll_temp
+    private Button bt_temparary; //For prototype
+    private Button bt_confirm_temp;
 
     private Project project;
 
@@ -55,12 +61,22 @@ public class CardActivity extends Activity implements View.OnClickListener{
         project = (Project) getIntent().getSerializableExtra("data");
 
         //initialize view variables
-        uvp_main = (UltraViewPager) findViewById(R.id.card_uvp_main);
-        pl_main = new PagerListener();
+        uvp_main = findViewById(R.id.card_uvp_main);
 
-        bt_image = (Button) findViewById(R.id.card_bt_image);
-        bt_text = (Button) findViewById(R.id.card_bt_text);
-        bt_temp = (Button) findViewById(R.id.card_bt_template);
+        bt_image = findViewById(R.id.card_bt_image);
+        bt_text = findViewById(R.id.card_bt_text);
+        bt_temp = findViewById(R.id.card_bt_template);
+
+        ll_text = findViewById(R.id.card_ll_control_text);
+        ll_temp = findViewById(R.id.card_ll_control_template);
+
+        et_text = findViewById(R.id.card_et_text);
+        et_title = findViewById(R.id.card_et_title);
+
+        bt_temparary = findViewById(R.id.card_bt_temp1);
+
+        bt_confirm_temp = findViewById(R.id.card_bt_confirm_temp);
+        bt_confirm_text = findViewById(R.id.card_bt_confirm_text);
 
         //initialize adapter
         cpa_main = new CardPagerAdapter(getApplicationContext(), new ArrayList<Card>());
@@ -69,7 +85,6 @@ public class CardActivity extends Activity implements View.OnClickListener{
     private void initView(){
         //Setting uvp_main
         uvp_main.setAdapter(cpa_main);
-        uvp_main.setOnPageChangeListener(pl_main);
 
         //uvp_main.initIndicator();
 
@@ -81,6 +96,11 @@ public class CardActivity extends Activity implements View.OnClickListener{
         bt_image.setOnClickListener(this);
         bt_text.setOnClickListener(this);
         bt_temp.setOnClickListener(this);
+
+        bt_temparary.setOnClickListener(this);
+
+        bt_confirm_text.setOnClickListener(this);
+        bt_confirm_temp.setOnClickListener(this);
     }
 
     @Override
@@ -88,7 +108,9 @@ public class CardActivity extends Activity implements View.OnClickListener{
         if (resultCode == RESULT_OK){
             if (requestCode == UCrop.REQUEST_CROP) {
                 Uri resultUri = UCrop.getOutput(data);
+
                 //TODO SOME?
+                cpa_main.getCurrentCard().setFileDir(resultUri.getPath());
             }
             else if(requestCode == REQUEST_PICK_PICTURE){
                 if(data != null)
@@ -126,21 +148,31 @@ public class CardActivity extends Activity implements View.OnClickListener{
                 requestImage();
                 break;
             case R.id.card_bt_text:
-                requestText();
+                openTextControl();
                 break;
             case R.id.card_bt_template:
-                requestTemplate();
+                openTemplateControl();
+                break;
+            case R.id.card_bt_confirm_temp:
+                closeTextControl();
+                break;
+            case R.id.card_bt_confirm_text:
+                closeTemplateControl();
+                break;
+            case R.id.card_bt_temp1:
+                cpa_main.getCurrentCard().setTemplate(0);
+                cpa_main.notifyDataSetChanged(); //TODO
                 break;
         }
     }
 
     private void deleteCard(){
-        cpa_main.getCards().remove(pl_main.getPosition());
+        cpa_main.removeCard();
         cpa_main.notifyDataSetChanged();
     }
 
     private void addCard(){
-        cpa_main.getCards().add(new Card(0, "", "", ""));
+        cpa_main.addCard();
         cpa_main.notifyDataSetChanged();
     }
 
@@ -148,12 +180,30 @@ public class CardActivity extends Activity implements View.OnClickListener{
         requestPick();
     }
 
-    private void requestText(){
+    private void openTextControl(){
+        ll_text.setVisibility(View.VISIBLE);
 
+        et_title.setText(cpa_main.getCurrentCard().getTitle());
+        et_text.setText(cpa_main.getCurrentCard().getText());
     }
 
-    private void requestTemplate(){
+    private void openTemplateControl(){
+        ll_temp.setVisibility(View.VISIBLE);
+    }
 
+    private void closeTextControl() {
+        ll_text.setVisibility(View.INVISIBLE);
+
+        Card c = cpa_main.getCurrentCard();
+        c.setTitle(et_title.getText().toString());
+        c.setText(et_text.getText().toString());
+
+        et_title.setText("");
+        et_text.setText("");
+    }
+
+    private void closeTemplateControl(){
+        ll_temp.setVisibility(View.INVISIBLE);
     }
 
     private void requestSave(){
@@ -191,7 +241,7 @@ public class CardActivity extends Activity implements View.OnClickListener{
 
     private void requestCrop(Uri uri){
 
-        String destinationFileName = "image"+pl_main.getPosition();
+        String destinationFileName = "image"+cpa_main.getCurrentPosition();
 
         UCrop uCrop = UCrop.of(uri,
                 Uri.fromFile(
@@ -229,27 +279,4 @@ public class CardActivity extends Activity implements View.OnClickListener{
         return Uri.fromFile(file);
     }
 
-    private class PagerListener implements ViewPager.OnPageChangeListener{
-
-        private int position = 0;
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            this.position = position;
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-
-        public int getPosition(){
-            return position;
-        }
-    }
 }
